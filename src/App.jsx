2b146f3cb0ff3840d5828f83905dc462eaeb13c5
Scarlet Sypher <ayushjha21157@gmail.com>
2026-03-20 11:33:22 +0530

    temp-work: add auto-redirect to deployed temp portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <h1 className="text-bold">This is wotrkin</h1>;
-};
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    window.location.href = "https://temp-portfolio-y7kh.vercel.app/";
+  }, []);
+
+  return null; // nothing renders
+}
 
 export default App;
